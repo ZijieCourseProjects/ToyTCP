9f13894992fc1915e58d61b12ef1b8ed3abdf947
colin_z <veweew@tju.edu.cn>
2022-09-12 16:27:26 +0800

    feat: Introduce a debug print macro

--- inc/debug.h
+++ inc/debug.h
@@ -6,11 +6,12 @@
 #define TJU_TCP_INC_DEBUG_H_
 
 #define DEBUG 1
+#include "stdio.h"
 
 //debug print macro with function name
 #define DEBUG_PRINT(...) \
     do {                 \
-        if(DEBUG){                 \
+        if(DEBUG){       \
         fprintf(stderr, "DEBUG MESSAGE: %s:%d:%s(): ", __FILE__, \
                 __LINE__, __func__); \
         fprintf(stderr, __VA_ARGS__);}             \
